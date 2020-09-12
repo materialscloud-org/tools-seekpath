076938b8866ddc37a2a77958247e5b45131615f5
Giovanni Pizzi <giovanni.pizzi@epfl.ch>
2020-09-12 11:05:38 +0200

    Fixes to the css and upped version numer
    - double-click to activate not working on JSMol
    - wrong spacing for jsmol options
    
    Version upped to 20.09.1

--- compute/seekpath_web_module.py
+++ compute/seekpath_web_module.py
@@ -17,7 +17,7 @@ from tools_barebone import logme, get_tools_barebone_version
 from tools_barebone.structure_importers import get_structure_tuple, UnknownFormatError
 
 # Version of tools-seekpath
-__version__ = "20.05.0"
+__version__ = "20.09.1"
 
 
 class FlaskRedirectException(Exception):
